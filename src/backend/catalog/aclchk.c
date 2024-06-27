9aab02ad2642604ebe7f60c51c5c7fd3906202a0
Peter Eisentraut <peter@eisentraut.org>
2024-06-27 14:10:44 +0200

    WIP: SQL Property Graph Queries (SQL/PGQ)
    Implementation of SQL property graph queries, according to SQL/PGQ
    standard (ISO 9075-16:2023).
    
    Author: Peter Eisentraut <peter@eisentraut.org>
    Author: Ashutosh Bapat <ashutosh.bapat.oss@gmail.com>
    Discussion: https://www.postgresql.org/message-id/flat/a855795d-e697-4fa5-8698-d20122126567@eisentraut.org
    
    Description of fixes added to the original patch
    
    1. Fixes suggested by pgperltidy.
    
    2. binary_upgrade_set_pg_class_oid() call fix: After
       6e1c4a03a978ed3574124d8f2be22ba2e5a4b1e9
       binary_upgrade_set_pg_class_oid() doesn't need the (now non-existent)
       last argument.
    
    3. decompile_column_index_array() call fix:
       89f908a6d0ac1337c868625008c9598487d184e7 added a new parameter
       withPeriod to decompile_column_index_array(). This function is used
       to decompile keys in property graph definition. KEYs in property
       graph definition are not temporal and thus do not need to specify
       PERIOD in their definition. Hence pass `false` to
       decompile_column_index_array() when called for while decompiling a
       property graph definition.
    
    4. Spurious column not found error: Attribute name from a heap tuple
       needs to be copied before releasing the resources held by the tuple
       and the table scan.
    
    5. Merge conflict fixes in parser.pl: after ECPG related refactoring
       from commits around 6b005499447512abfa1e4add87dcf3e2859c4f9e.
    
    6. Merge conflict in alter.c and aclchk.c when merging with HEAD =
       aac831cafa6f3106dfcbd3298757801c299351fc
    
    7. Fix compilation error in propgraph_element_get_key():
    RelationGetPrimaryKeyIndex(), called by propgraph_element_get_key() requires a
    second argument specifying whether DEFERRED primary key constraints are
    acceptable. SQL/PGQ standard section 9.12, Syntax Rule 4 mentions only primary
    key and does not discuss DEFERREDness. Fix compilation failure by passing "true"
    to RelationGetPrimaryKeyIndex() as second argument. No adjustment to the
    documentation is needed since it mentioned primary key without DEFERREDness.
    
    8. Merge conflict fixes to gram.y, psql-ref.sgml, psql/help.c,
       primnodes.h when merging with HEAD =
       fd4c4ede708fc5affb04362bb5a8bdc3136fa963

--- src/backend/catalog/aclchk.c
+++ src/backend/catalog/aclchk.c
@@ -291,6 +291,9 @@ restrict_and_check_grant(bool is_grant, AclMode avail_goptions, bool all_privs,
 		case OBJECT_PARAMETER_ACL:
 			whole_mask = ACL_ALL_RIGHTS_PARAMETER_ACL;
 			break;
+		case OBJECT_PROPGRAPH:
+			whole_mask = ACL_ALL_RIGHTS_PROPGRAPH;
+			break;
 		default:
 			elog(ERROR, "unrecognized object type: %d", objtype);
 			/* not reached, but keep compiler quiet */
@@ -535,6 +538,10 @@ ExecuteGrantStmt(GrantStmt *stmt)
 			all_privileges = ACL_ALL_RIGHTS_PARAMETER_ACL;
 			errormsg = gettext_noop("invalid privilege type %s for parameter");
 			break;
+		case OBJECT_PROPGRAPH:
+			all_privileges = ACL_ALL_RIGHTS_PROPGRAPH;
+			errormsg = gettext_noop("invalid privilege type %s for property graph");
+			break;
 		default:
 			elog(ERROR, "unrecognized GrantStmt.objtype: %d",
 				 (int) stmt->objtype);
@@ -605,6 +612,7 @@ ExecGrantStmt_oids(InternalGrant *istmt)
 	{
 		case OBJECT_TABLE:
 		case OBJECT_SEQUENCE:
+		case OBJECT_PROPGRAPH:
 			ExecGrant_Relation(istmt);
 			break;
 		case OBJECT_DATABASE:
@@ -687,6 +695,7 @@ objectNamesToOids(ObjectType objtype, List *objnames, bool is_grant)
 
 		case OBJECT_TABLE:
 		case OBJECT_SEQUENCE:
+		case OBJECT_PROPGRAPH:
 
 			/*
 			 * Here, we don't use get_object_address().  It requires that the
@@ -802,6 +811,10 @@ objectsInSchemaToOids(ObjectType objtype, List *nspnames)
 				break;
 			case OBJECT_SEQUENCE:
 				objs = getRelationsInNamespace(namespaceId, RELKIND_SEQUENCE);
+				objects = list_concat(objects, objs);
+				break;
+			case OBJECT_PROPGRAPH:
+				objs = getRelationsInNamespace(namespaceId, RELKIND_PROPGRAPH);
 				objects = list_concat(objects, objs);
 				break;
 			case OBJECT_FUNCTION:
@@ -1004,6 +1017,10 @@ ExecAlterDefaultPrivilegesStmt(ParseState *pstate, AlterDefaultPrivilegesStmt *s
 		case OBJECT_SCHEMA:
 			all_privileges = ACL_ALL_RIGHTS_SCHEMA;
 			errormsg = gettext_noop("invalid privilege type %s for schema");
+			break;
+		case OBJECT_PROPGRAPH:
+			all_privileges = ACL_ALL_RIGHTS_PROPGRAPH;
+			errormsg = gettext_noop("invalid privilege type %s for property graph");
 			break;
 		default:
 			elog(ERROR, "unrecognized GrantStmt.objtype: %d",
@@ -2699,6 +2716,9 @@ aclcheck_error(AclResult aclerr, ObjectType objtype,
 					case OBJECT_PROCEDURE:
 						msg = gettext_noop("permission denied for procedure %s");
 						break;
+					case OBJECT_PROPGRAPH:
+						msg = gettext_noop("permission denied for property graph %s");
+						break;
 					case OBJECT_PUBLICATION:
 						msg = gettext_noop("permission denied for publication %s");
 						break;
@@ -2824,6 +2844,9 @@ aclcheck_error(AclResult aclerr, ObjectType objtype,
 						break;
 					case OBJECT_PROCEDURE:
 						msg = gettext_noop("must be owner of procedure %s");
+						break;
+					case OBJECT_PROPGRAPH:
+						msg = gettext_noop("must be owner of property graph %s");
 						break;
 					case OBJECT_PUBLICATION:
 						msg = gettext_noop("must be owner of publication %s");
@@ -2961,6 +2984,7 @@ pg_aclmask(ObjectType objtype, Oid object_oid, AttrNumber attnum, Oid roleid,
 				pg_attribute_aclmask(object_oid, attnum, roleid, mask, how);
 		case OBJECT_TABLE:
 		case OBJECT_SEQUENCE:
+		case OBJECT_PROPGRAPH:
 			return pg_class_aclmask(object_oid, roleid, mask, how);
 		case OBJECT_DATABASE:
 			return object_aclmask(DatabaseRelationId, object_oid, roleid, mask, how);
