b9f43055f24ecd80ce470024915a30ed5b0aa8ab
Your Name <email@example.com>
2024-08-06 10:08:34 +0000

    fix stale comments
    PartitionCmd is used by ALTER TABLE/INDEX ATTACH/DETACH/MERGE/SPLIT,
    the comments do not reflect that.
    
    Signed-off-by: Your Name <email@example.com>

--- src/include/nodes/parsenodes.h
+++ src/include/nodes/parsenodes.h
@@ -950,12 +950,13 @@ typedef struct SinglePartitionSpec
 } SinglePartitionSpec;
 
 /*
- * PartitionCmd - info for ALTER TABLE/INDEX ATTACH/DETACH PARTITION commands
+ * PartitionCmd - info for ALTER TABLE/INDEX ATTACH/DETACH/MERGE/SPLIT PARTITION commands
  */
 typedef struct PartitionCmd
 {
 	NodeTag		type;
-	RangeVar   *name;			/* name of partition to attach/detach */
+	RangeVar   *name;			/* name of partition to
+								 * attach/detach/merge/split */
 	PartitionBoundSpec *bound;	/* FOR VALUES, if attaching */
 	List	   *partlist;		/* list of partitions, for MERGE/SPLIT
 								 * PARTITION command */
